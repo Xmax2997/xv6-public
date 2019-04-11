89cac76df800d138a4d9cdabf7d9586dcc3f1764
Xmax2997 <maxqm.2997@gmail.com>
2019-04-10 20:06:19 -0400

    Tarea 1

--- getprocs.c
+++ getprocs.c
@@ -1,15 +1,7 @@
-#include "types.h"
-#include "defs.h"
-#include "param.h"
-#include "memlayout.h"
-#include "mmu.h"
-#include "proc.h"
-#include "x86.h"
+#include <stdio.h>
 
 int
 main(void)
 {
-	cprintf("Hay %d procesos en ejecución en la CPU", getprocs());
-	return 0; 
-
+	printf("Hay %d procesos en ejecución en la CPU", getprocs());
 }
