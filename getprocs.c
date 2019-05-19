314654c71a96b4da90c35d87d35bcd95adfb8be8
Xmax2997 <maxqm.2997@gmail.com>
2019-05-19 12:40:08 -0400

    test13

--- getprocs.c
+++ getprocs.c
@@ -1,6 +1,5 @@
-
-#include <stdlib.h>
+#include "defs.h"
 
 int main(void){
-printf("La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
+cprintf("La cantidad de procesos en ejecucion en la CPU es %d\n", getprocs());
 }
